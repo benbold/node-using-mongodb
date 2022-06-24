5c25c70da6021dd056afe2574bf8c4f3cd4f4ad5
benoit.bolduc <benoit.bolduc@logibec.com>
2022-06-24 14:26:34 -0400

    mongoose setup

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 4000;
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/productsdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 app.get('/', (req, res) =>
     res.send(`Store server running on port ${PORT}`)
 );
 
-app.listen(PORT, () => 
+app.listen(PORT, () =>
     console.log(`Your server is running on port ${PORT}`)
 );
